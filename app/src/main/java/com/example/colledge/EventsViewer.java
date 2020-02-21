ddba119f5e3063650bc6ed92539f245fa0d7a8bd
sj1young <56085453+sj1young@users.noreply.github.com>
2020-02-21 16:25:04 -0500

    hds

---
+++ app/src/main/java/com/example/colledge/EventsViewer.java
@@ -0,0 +1,14 @@
+package com.example.colledge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventsViewer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events_viewer);
+    }
+}
---
+++ app/src/main/java/com/example/colledge/GPASuiteHome.java
@@ -0,0 +1,14 @@
+package com.example.colledge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GPASuiteHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gpasuite_home);
+    }
+}
